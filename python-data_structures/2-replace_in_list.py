16176cac5f7486828a1d48e37a9aa5021e2b9893
Chaospossum <duqni042@gmail.com>
2026-05-04 13:11:34 +0000

    Add task 2: replace_in_list

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+def replace_in_list(my_list, idx, element):
+    def replace_in_list(my_list, idx, element):
+    """Replace element at idx in my_list,or return list unchanged if invalid."""
+    if idx < 0 or idx >0 len(my_list): 
+        return my_list my_list[idx] = element
+    return my_list

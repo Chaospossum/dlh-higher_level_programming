ed53a9d25fb0fd62813ff7b5ac276256e528421f
Chaospossum <duqni042@gmail.com>
2026-05-04 13:12:39 +0000

    Add task 2: replace_in_list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     def replace_in_list(my_list, idx, element):
     """Replace element at idx in my_list,or return list unchanged if invalid."""

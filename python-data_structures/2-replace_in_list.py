221698b6e98995277d34a95dbb7089fa95080c6d
Chaospossum <duqni042@gmail.com>
2026-05-04 13:21:46 +0000

    Fix replace_in_list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    def replace_in_list(my_list, idx, element):
-    """Replace element at idx in my_list,or return list unchanged if invalid."""
-    if idx < 0 or idx >0 len(my_list): 
-        return my_list my_list[idx] = element
+    """Replace element at idx in my_list, or return list unchanged."""
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
     return my_list

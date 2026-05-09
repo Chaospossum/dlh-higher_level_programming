13c5df3c7edac9a95d5ab82a74a3b0ef8e7a5c88
Chaospossum <duqni042@gmail.com>
2026-05-09 19:47:57 +0000

    Add README.md and fix 0-square_matrix_simple with pycodestyle compliance

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module for square_matrix_simple method."""
+
+
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
+    """Computes the square of all integers in a matrix."""
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
---
+++ python-more_data_structures/README.md
@@ -0,0 +1,3 @@
+# Python - More Data Structures: Set, Dictionary
+
+Exercises on sets, dictionaries, lambda, map, filter, and reduce.

f29f92216baff40416fd2332317ab5001eeb717b
Chaospossum <duqni042@gmail.com>
2026-05-09 19:43:53 +0000

    Fix 0 and 11 to 3-line style; add 12-roman_to_int

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-"""Module for square_matrix_simple method."""
-
-
 def square_matrix_simple(matrix=[]):
-    """Computes the square value of all integers of a matrix."""
-    return [list(map(lambda x: x ** 2, row)) for row in matrix]
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
